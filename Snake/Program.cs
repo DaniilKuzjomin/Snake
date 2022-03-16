97fd3ff0df211ea4795dfb2b5c0c5e5ab43babcc
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2022-03-16 08:54:58 +0200

    Update Program.cs

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -52,19 +52,6 @@ namespace Snake
 			Console.ReadLine();
 		}
 
-
-		static void WriteGameOver()
-		{
-			int xOffset = 25;
-			int yOffset = 8;
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.SetCursorPosition(xOffset, yOffset++);
-			WriteText("============================", xOffset, yOffset++);
-			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
-			yOffset++;
-			WriteText("Автор: Daniil Kuzjomin", xOffset + 2, yOffset++);
-			WriteText("При помощи GeekBrains", xOffset + 1, yOffset++);
-			WriteText("============================", xOffset, yOffset++);
 		}
 
 		static void WriteText(String text, int xOffset, int yOffset)
