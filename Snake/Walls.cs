a83fc7281d2375f783ef1dba88c58963c8595da5
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2022-03-20 22:17:27 +0200

    Comment

--- Snake/Walls.cs
+++ Snake/Walls.cs
@@ -10,35 +10,36 @@ namespace Snake
 	{
 		List<Figure> wallList;
 
-		public Walls(int mapWidth, int mapHeight)
+		public Walls(int mapWidth, int mapHeight) // Конструктор для понятия габаритов карты
 		{
 			wallList = new List<Figure>();
 
-			// Отрисовка рамки
+			// Отрисовка рамки ( рисует линии по габаритам )
 			HorizontalLine upLine = new HorizontalLine(0, mapWidth - 2, 0, '+');
 			HorizontalLine downLine = new HorizontalLine(0, mapWidth - 2, mapHeight - 1, '+');
 			VerticalLine leftLine = new VerticalLine(0, mapHeight - 1, 0, '+');
 			VerticalLine rightLine = new VerticalLine(0, mapHeight - 1, mapWidth - 2, '+');
 
+			// Добавление данных линий
 			wallList.Add(upLine);
 			wallList.Add(downLine);
 			wallList.Add(leftLine);
 			wallList.Add(rightLine);
 		}
 
-		internal bool IsHit(Figure figure)
+		internal bool IsHit(Figure figure) // Функция для проверки соприкосновение змейки со стеной
 		{
 			foreach (var wall in wallList)
 			{
-				if (wall.IsHit(figure))
+				if (wall.IsHit(figure)) // Если столкнулась, то возвращается True
 				{
 					return true;
 				}
 			}
-			return false;
+			return false; // В другом случае, возвращаем False
 		}
 
-		// Метод отрисовки стен
+		// Метод отрисовки 
 		public void Draw()
 		{
 			foreach (var wall in wallList)
